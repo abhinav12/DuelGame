43bf75eb65e0afa874d3ceaa0e41411b4ca9d588
Nisha Agrawal <niagrawa@gmail.com>
2018-07-25 16:17:46 +0530

    Assets cursor changes

---
+++ DuelAR/Assets/Scripts/OpponentCommands.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class OpponentCommands : MonoBehaviour
+{
+	public GameObject scoreBoard;
+
+	private void Awake()
+	{
+		scoreBoard.GetComponentsInChildren<TextMesh>()[0].text = "100";
+	}
+	// Called by GazeGestureManager when the user performs a Select gesture
+	void OnSelect()
+	{
+		string oldScoreStr = scoreBoard.GetComponentsInChildren<TextMesh>()[0].text;
+		int oldScore = int.Parse(oldScoreStr);
+		int newScore = oldScore - 10;
+		string newScoreStr = newScore.ToString();
+		scoreBoard.GetComponentsInChildren<TextMesh>()[0].text = newScoreStr;
+	}
+}
